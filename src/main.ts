09a32aea6a36ab1df1da69845cc79c0a57663bc8
Wesley <147221712+Wesleyweb30@users.noreply.github.com>
2025-01-19 00:27:09 -0300

    deploy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://task-management-app-angular.onrender.com/', // URL da aplicação Angular
+    origin: 'https://task-management-app-angular.onrender.com', // URL da aplicação Angular
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Caso você use cookies
   });
