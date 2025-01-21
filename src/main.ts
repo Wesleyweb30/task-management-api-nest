fcce50bb3d702d9105d1fd0c49b95ed6b2b0b37b
Wesley <147221712+Wesleyweb30@users.noreply.github.com>
2025-01-21 16:13:06 -0300

    Open cros in app local and deply

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://task-management-app-angular.onrender.com', // URL da aplicação Angular
+    origin: [
+      'https://task-management-app-angular.onrender.com', 
+      'http://localhost:4200' 
+    ] , 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Caso você use cookies
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
